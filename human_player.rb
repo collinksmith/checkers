cef1a319e236df630e85c9b2ac9995faa4168c0f
Collin Smith <collink33@gmail.com>
2015-07-02 13:37:51 -0700

    Add to_s

--- human_player.rb
+++ human_player.rb
@@ -1,11 +1,17 @@
 require 'io/console'
 
 class HumanPlayer
+  attr_reader :color
+
   def initialize(color)
     @color = color
   end
 
   def get_input
     $stdin.getch
+  end
+
+  def to_s
+    color.to_s
   end
 end
