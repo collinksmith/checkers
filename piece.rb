4f399e570f0d7cfead8b4a86e39c7c44a00161e2
Collin Smith <collink33@gmail.com>
2015-07-02 11:50:00 -0700

    Add perform_move skeleton

--- piece.rb
+++ piece.rb
@@ -1,7 +1,7 @@
 require 'colorize'
 
 class Piece
-  attr_reader :color
+  attr_reader :color, :pos, :kinged
 
   def initialize(color, pos)
     @color = color
@@ -17,7 +17,34 @@ class Piece
     color == :white ? "\u26AA" : "\u26AB"
   end
 
-  def perform_slide()
+  def perform_move(end_pos)
 
+  end
+
+  def perform_slide(end_pos)
+    if valid_slide?(end_pos)
+      board.move(pos, end_pos)
+    end
+  end
+
+  def perform_jump(end_pos)
+    if valid_jump?(end_pos)
+      board.move(pos, end_pos)
+    end
+  end
+
+  def king?
+    kinged
+  end
+
+  private
+
+  def valid_slide?(end_pos)
+
+
+  end
+
+  def on_board?(pos)
+    pos
   end
 end
