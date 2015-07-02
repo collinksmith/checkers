378d2e6e46d7ce1cb35054be332df3060b8a7251
Collin Smith <collink33@gmail.com>
2015-07-02 11:22:27 -0700

    Add board to game and add current_player

--- game.rb
+++ game.rb
@@ -1,13 +1,22 @@
+require_relative 'board'
+
 class Game
+  attr_reader :board, :players
+
   def initialize(player1, player2)
     @players = [player1, player2]
+    @board = Board.new(8)
   end
 
   def play
     loop do
-      input = layer.get_input
+      input = current_player.get_input
       process_input(input)
     end
+  end
+
+  def current_player
+    players.first
   end
 
   def process_input(input)
